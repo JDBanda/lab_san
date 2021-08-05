73f49baa6d2e15bff7b7eed2be2323ff28b6b497
JDBanda <jbandav@itsoeh.edu.mx>
2021-08-05 17:15:05 -0500

    Corrections in model

--- beta/venta/models.py
+++ beta/venta/models.py
@@ -4,12 +4,15 @@ from django.db import models
 
 # Tablas básicas
 
+class Sucursal(models.Model):
+    sucu_nombre = models.CharField("Nombre de sucursal", max_length=100)
+    sucu_especialidad = models.CharField("Especialidad", max_length=100)
 
 class Paciente(models.Model):
-    paci_apellidoPaterno = models.CharField("Apellido paterno", max_length=100)
-    paci_apellidoMaterno = models.CharField("Apellido materno", max_length=100)
-    paci_nombre = models.CharField("Nombre(s)", max_length=250)
-    paci_direccion = models.CharField("Dirección", max_length=250)
+    paci_apellidoPaterno = models.CharField("Apellido paterno", max_length=30)
+    paci_apellidoMaterno = models.CharField("Apellido materno", max_length=30)
+    paci_nombre = models.CharField("Nombre(s)", max_length=50)
+    paci_direccion = models.CharField("Dirección", max_length=100)
     paci_localidad = models.CharField("Localidad", max_length=100)
     paci_pais = models.CharField("País", max_length=100)
     paci_estado = models.CharField("Estado", max_length=100)
@@ -24,10 +27,10 @@ class Paciente(models.Model):
 
 
 class Medico(models.Model):
-    medi_apellidoPaterno = models.CharField("Apellido paterno", max_length=100)
-    medi_apellidoMaterno = models.CharField("Apellido materno", max_length=100)
-    medi_nombre = models.CharField("Nombre(s)", max_length=250)
-    medi_direccion = models.CharField("Dirección", max_length=250)
+    medi_apellidoPaterno = models.CharField("Apellido paterno", max_length=30)
+    medi_apellidoMaterno = models.CharField("Apellido materno", max_length=30)
+    medi_nombre = models.CharField("Nombre(s)", max_length=50)
+    medi_direccion = models.CharField("Dirección", max_length=150)
     medi_localidad = models.CharField("Localidad", max_length=100)
     medi_pais = models.CharField("País", max_length=100)
     medi_estado = models.CharField("Estado", max_length=100)
@@ -44,10 +47,10 @@ class Medico(models.Model):
 
 
 class Usuario(models.Model):
-    usua_apellidoPaterno = models.CharField(max_length=100)
-    usua_apellidoMaterno = models.CharField(max_length=100)
-    usua_nombre = models.CharField(max_length=250)
-    usua_direccion = models.CharField(max_length=250)
+    usua_apellidoPaterno = models.CharField(max_length=30)
+    usua_apellidoMaterno = models.CharField(max_length=30)
+    usua_nombre = models.CharField(max_length=50)
+    usua_direccion = models.CharField(max_length=150)
     usua_localidad = models.CharField(max_length=100)
     usua_pais = models.CharField(max_length=100)
     usua_estado = models.CharField(max_length=100)
@@ -64,21 +67,21 @@ class Usuario(models.Model):
 
 
 class Categoria(models.Model):
-    cate_nombre = models.CharField("Categoria", max_length=250)
+    cate_nombre = models.CharField("Categoria", max_length=100)
 
     def __str__(self):
         return self.cate_nombre
 
 
 class modoPago(models.Model):
-    modo_nombre = models.CharField("Modo de pago", max_length=250)
+    modo_nombre = models.CharField("Modo de pago", max_length=150)
 
     def __str__(self):
         return self.modo_nombre
 
 
 class Status(models.Model):
-    status_nombre = models.CharField("Estatus", max_length=250)
+    status_nombre = models.CharField("Estatus", max_length=150)
 
     def __str__(self):
         return self.status_nombre
@@ -87,12 +90,12 @@ class Status(models.Model):
 
 
 class Servicio(models.Model):
-    serv_nombre = models.CharField("Nombre del servicio", max_length=250)
+    serv_nombre = models.CharField("Nombre del servicio", max_length=150)
     serv_categoria = models.ForeignKey(
         Categoria, null=True, on_delete=models.SET_NULL)
     serv_precio = models.FloatField("Precio")
     serv_iva = models.FloatField("IVA")
-    serv_codigobarra = models.CharField("Codigo de barras", max_length=250)
+    serv_codigobarra = models.CharField("Codigo de barras", max_length=150)
 
     def __str__(self):
         return self.serv_nombre
@@ -102,13 +105,13 @@ class NotaDeVenta(models.Model):
     nota_idPaciente = models.ForeignKey(
         Paciente, null=True, on_delete=models.SET_NULL)
     # Pendiente por que no se acceder a los usuarios
-    nota_idUsuario = models.CharField(max_length=250, null=True)
+    nota_idUsuario = models.CharField(max_length=150, null=True)
     nota_fecha = models.DateTimeField("Fecha")
     nota_statusPago = models.ForeignKey(
         Status, null=True, on_delete=models.SET_NULL)
     nota_modoPago = models.ForeignKey(
         modoPago, null=True, on_delete=models.SET_NULL)
-    nota_entrega = models.CharField("quien recibe", max_length=250)
+    nota_entrega = models.CharField("quien recibe", max_length=150)
     nota_pago = models.FloatField(null=True)
     nota_total = models.FloatField(null=True)
 
